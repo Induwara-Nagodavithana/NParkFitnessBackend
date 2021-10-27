addb824f04df0bc702c520d2719f7dcdfe9ce41e
Induwara-Nagodavithana <induwara.nagodavithana@gmail.com>
2021-10-27 13:10:38 +0530

    delete changed

--- app/route/user.route.js
+++ app/route/user.route.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { upload, getFileStream } = require('../config/aws-s3');
 const UserController = require('../controller/user.controller');
 const UserRouter = express.Router();
 
@@ -15,6 +16,18 @@ UserRouter.get('/', UserController.getAllUser);
 UserRouter.post('/', UserController.createUser);
 UserRouter.put('/:id', UserController.updateUser);
 UserRouter.post('/validate', UserController.validateUser);
-UserRouter.delete('/:id',UserController.deleteUser);
+UserRouter.delete('/:id', UserController.deleteUser);
+UserRouter.post('/upload', upload.single('image'), function (req, res, next) {
+    console.log(req.file)
+
+    res.send('Successfully uploaded ' + req.file.key + ' files!')
+})
+UserRouter.get('/images/:key', (req, res) => {
+    console.log(req.params)
+    const key = req.params.key
+    const readStream = getFileStream(key)
+
+    readStream.pipe(res)
+})
 
 module.exports = UserRouter;
