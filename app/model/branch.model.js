a679fad22f34233be3762eb5efd69e8adb3830d9
Induwara-Nagodavithana <induwara.nagodavithana@gmail.com>
2022-02-22 12:03:08 +0530

    branch model changed

--- app/model/branch.model.js
+++ app/model/branch.model.js
@@ -3,41 +3,44 @@ const Sequelize = require("sequelize");
 const db = require("../config/database");
 
 var Branch = db.define(
-    "branch",
-    {
-        name: {
-            type: Sequelize.STRING,
-            allowNull: false,
-            unique: true
-        },
-        // address: {
-        //     type: Sequelize.STRING,
-        //     allowNull: false,
-        // }
-        street: {
-            type: Sequelize.STRING,
-            allowNull: false,
-        },
-        lane: {
-            type: Sequelize.STRING,
-            allowNull: true,
-        },
-        city: {
-            type: Sequelize.STRING,
-            allowNull: false,
-        },
-        province: {
-            type: Sequelize.STRING,
-            allowNull: false,
-        },
+  "branch",
+  {
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
     },
-    {
-        timestamps: true,
-        createdAt: "CreatedAt",
-        updatedAt: "UpdatedAt",
-        freezeTableName: true,
-    }
+    isActive: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+    },
+    // address: {
+    //     type: Sequelize.STRING,
+    //     allowNull: false,
+    // }
+    street: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    lane: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    city: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    province: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: true,
+    createdAt: "CreatedAt",
+    updatedAt: "UpdatedAt",
+    freezeTableName: true,
+  }
 );
 
-
-module.exports = Branch ;
+module.exports = Branch;
