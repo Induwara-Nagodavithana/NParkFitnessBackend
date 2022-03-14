86d8048d16d84a8cedf284a85d5e35330078afdb
Induwara-Nagodavithana <induwara.nagodavithana@gmail.com>
2022-03-14 16:38:25 +0530

    database error fixed

--- app/config/database.js
+++ app/config/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
 // Order is 'database', 'username', 'password'
-db = new Sequelize("gymapp", "admin", "123456789", {
+db = new Sequelize("gymapp", "admin", "Wasd#2731", {
   host: "database-2.clwjrzzurrw6.us-east-2.rds.amazonaws.com",
   port: 3306,
   dialect: "mysql",
