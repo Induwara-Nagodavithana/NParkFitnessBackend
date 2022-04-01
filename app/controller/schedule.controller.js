d2027ee3ddf2d04ffaa864f9e9c61597f71c0b6e
Induwara-Nagodavithana <induwara.nagodavithana@gmail.com>
2022-04-01 18:46:32 +0530

    New Api Added

--- app/controller/schedule.controller.js
+++ app/controller/schedule.controller.js
@@ -1,122 +1,151 @@
 const Schedule = require("../model/schedule.model");
 const User = require("../model/user.model");
 const Membership = require("../model/membership.model");
+const { Sequelize, Op } = require("sequelize");
 
 //Register a Schedule | guest
 exports.createSchedule = async (req, res) => {
-    if (req.body) {
-        console.log("Create schedule");
-        Schedule.create(req.body)
-            .then((schedule) => {
-                res.send({
-                    'success': 'true',
-                    'data': schedule
-                });
-            })
-            .catch((err) => {
-                res.status(400).send({
-                    'success': 'false',
-                    'message': 'Error in Create Schedule',
-                    'description': err.message
-                });
-            });
-    }
-}
-
+  if (req.body) {
+    console.log("Create schedule");
+    Schedule.create(req.body)
+      .then((schedule) => {
+        res.send({
+          success: "true",
+          data: schedule,
+        });
+      })
+      .catch((err) => {
+        res.status(400).send({
+          success: "false",
+          message: "Error in Create Schedule",
+          description: err.message,
+        });
+      });
+  }
+};
 
 //update Schedule Details
 exports.updateSchedule = async (req, res) => {
-    if (req.body) {
-        if (!req.params.id) return res.status(500).send("Id is missing");
-        let id = req.params.id;
-        Schedule.update(req.body, {
-            where: {
-                id: id,
-            },
-        })
-            .then((schedule) => {
-                res.status(200).send({
-                    'success': schedule[0] == 1 ? 'true' : 'false',
-                    'data': schedule[0] == 1 ? "Updated Successfully" : "Update Not Successful"
-                });
-            })
-            .catch((err) => {
-                res.status(400).send({
-                    'success': 'false',
-                    'message': 'Error in Update Schedule',
-                    'description': err.message
-                });
-            });
-    }
-}
-
+  if (req.body) {
+    if (!req.params.id) return res.status(500).send("Id is missing");
+    let id = req.params.id;
+    Schedule.update(req.body, {
+      where: {
+        id: id,
+      },
+    })
+      .then((schedule) => {
+        res.status(200).send({
+          success: schedule[0] == 1 ? "true" : "false",
+          data:
+            schedule[0] == 1 ? "Updated Successfully" : "Update Not Successful",
+        });
+      })
+      .catch((err) => {
+        res.status(400).send({
+          success: "false",
+          message: "Error in Update Schedule",
+          description: err.message,
+        });
+      });
+  }
+};
 
 //get All Schedule
 exports.getAllSchedule = (req, res) => {
-    console.log("get All");
-    Schedule.findAll().then((schedule) => {
-        res.send({
-            'success': 'true',
-            'data': schedule
-        });
+  console.log("get All");
+  Schedule.findAll()
+    .then((schedule) => {
+      res.send({
+        success: "true",
+        data: schedule,
+      });
     })
-        .catch((err) => {
-            res.status(400).send({
-                'success': 'false',
-                'message': 'Error in Getting All Schedule',
-                'description': err.message
-            });
-        });
-}
+    .catch((err) => {
+      res.status(400).send({
+        success: "false",
+        message: "Error in Getting All Schedule",
+        description: err.message,
+      });
+    });
+};
 
 //get Schedule By Id
 exports.getScheduleById = (req, res) => {
-    console.log("get All");
-    Schedule.findOne({
-        where: {
-            id: req.params.id
-        },
-        include: [{
-            model: User,
-            as: 'trainer'
-        }, {
-            model: Membership
-        }]
-    }).then((schedule) => {
-        res.send({
-            'success': 'true',
-            'data': schedule
-        });
+  console.log("get All");
+  Schedule.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: User,
+        as: "trainer",
+      },
+      {
+        model: Membership,
+      },
+    ],
+  })
+    .then((schedule) => {
+      res.send({
+        success: "true",
+        data: schedule,
+      });
     })
-        .catch((err) => {
-            res.status(400).send({
-                'success': 'false',
-                'message': 'Error in Getting Schedule By ID',
-                'description': err.message
-            });
-        });
-}
+    .catch((err) => {
+      res.status(400).send({
+        success: "false",
+        message: "Error in Getting Schedule By ID",
+        description: err.message,
+      });
+    });
+};
+
+//get Schedule Count By TrainerId And Month
+exports.getScheduleCountByTrainerIdAndMonth = (req, res) => {
+  console.log("get All");
+  Schedule.count({
+    where: {
+      trainerId: req.params.id,
+      CreatedAt: { [Op.startsWith]: req.body.month },
+    },
+  })
+    .then((schedule) => {
+      res.send({
+        success: "true",
+        data: schedule,
+      });
+    })
+    .catch((err) => {
+      res.status(400).send({
+        success: "false",
+        message: "Error in Getting Schedule By ID",
+        description: err.message,
+      });
+    });
+};
 
 //delete Schedule
 exports.deleteSchedule = async (req, res) => {
-    console.log("Delete schedule");
-    Schedule.destroy({
-        where: {
-            id: req.params.id
-        }
+  console.log("Delete schedule");
+  Schedule.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((schedule) => {
+      console.log(schedule);
+      res.status(200).send({
+        success: schedule == 1 ? "true" : "false",
+        data: schedule == 1 ? "Deleted Successfully" : "Delete Not Successful",
+      });
     })
-        .then((schedule) => {
-            console.log(schedule)
-            res.status(200).send({
-                'success': schedule == 1 ? 'true' : 'false',
-                'data': schedule == 1 ? "Deleted Successfully" : "Delete Not Successful"
-            });
-        })
-        .catch((err) => {
-            res.status(400).send({
-                'success': 'false',
-                'message': 'Error in Delete Schedule',
-                'description': err.message
-            });
-        });
-}
+    .catch((err) => {
+      res.status(400).send({
+        success: "false",
+        message: "Error in Delete Schedule",
+        description: err.message,
+      });
+    });
+};
--- app/route/schedule.route.js
+++ app/route/schedule.route.js
@@ -6,6 +6,7 @@ const ScheduleController = require('../controller/schedule.controller');
     ScheduleRouter.get('/', ScheduleController.getAllSchedule);
     ScheduleRouter.post('/', ScheduleController.createSchedule);
     ScheduleRouter.get('/:id', ScheduleController.getScheduleById);
+    ScheduleRouter.post('/getScheduleCountByTrainerIdAndMonth/:id', ScheduleController.getScheduleCountByTrainerIdAndMonth);
     ScheduleRouter.put('/:id', ScheduleController.updateSchedule);
     ScheduleRouter.delete('/:id',ScheduleController.deleteSchedule);
     module.exports = ScheduleRouter;
