81931c2071347c3556956f4a0c19167ace49927b
Induwara-Nagodavithana <induwara.nagodavithana@gmail.com>
2022-06-15 21:52:03 +0530

    add new api

--- app/route/auth.route.js
+++ app/route/auth.route.js
@@ -1,14 +1,20 @@
-const express = require('express');
-const UserController = require('../controller/user.controller');
+const express = require("express");
+const UserController = require("../controller/user.controller");
+const SubscriptionTypeController = require("../controller/subscriptionType.controller");
 const AuthRouter = express.Router();
-
 
 // AuthRouter.get('/', UserController.getAllUser);
 // AuthRouter.post('/', UserController.createUser);
 // AuthRouter.put('/:id', UserController.updateUser);
-AuthRouter.post('/validate', UserController.validateUser);
-AuthRouter.post('/validateUserByFireUIDAndEmail', UserController.validateUserByFireUIDAndEmail);
-AuthRouter.post('/signUp', UserController.createUser);
-
+AuthRouter.post("/validate", UserController.validateUser);
+AuthRouter.post(
+  "/validateUserByFireUIDAndEmail",
+  UserController.validateUserByFireUIDAndEmail
+);
+AuthRouter.post("/signUp", UserController.createUser);
+AuthRouter.get(
+  "/getAllSubscriptionTypes",
+  SubscriptionTypeController.getAllSubscriptionType
+);
 
 module.exports = AuthRouter;
